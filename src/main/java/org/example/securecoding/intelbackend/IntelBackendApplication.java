3c1b9bd97f2327a3159b152546fdec516765f919
DustInTheWind <arw.prt@gmail.com>
2026-03-09 15:42:52 +0700

    init

---
+++ src/main/java/org/example/securecoding/intelbackend/IntelBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.securecoding.intelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/securecoding/intelbackend/IntelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securecoding.intelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
