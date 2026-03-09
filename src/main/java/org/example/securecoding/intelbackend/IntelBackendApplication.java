2d6c08c5271ac9bef235a21d5d732dd1a75662f4
DustInTheWind <arw.prt@gmail.com>
2026-03-09 20:33:52 +0700

    lmao udah jadi ternyata

--- src/main/java/org/example/securecoding/intelbackend/IntelBackendApplication.java
+++ src/main/java/org/example/securecoding/intelbackend/IntelBackendApplication.java
@@ -1,7 +1,5 @@
 package org.example.securecoding.intelbackend;
 
-import org.example.securecoding.intelbackend.shallow.InsecureBusinessCardAPI;
-import org.example.securecoding.intelbackend.shallow.InsecureUserSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IntelBackendApplication {
 
     public static void main(String[] args) {
-        InsecureBusinessCardAPI api = new InsecureBusinessCardAPI();
-        InsecureUserSession session = new InsecureUserSession("user1", api.generateAnonymousToken(), true);
-
-        System.out.println(session.getUsername());
-        System.out.println(session.getMsalToken());
-
-        System.out.println(api.getEmployeeData(session.getMsalToken(), "what i do"));
-
-//        SpringApplication.run(IntelBackendApplication.class, args);
+        SpringApplication.run(IntelBackendApplication.class, args);
     }
-
 }
